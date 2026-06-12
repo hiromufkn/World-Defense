37689320948637768e114025131c91f72318b3ed
R1806 <student@KIC-GAME-C35>
2026-06-12 13:48:03 +0900

    追加

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,7 +6,9 @@ public class Player : MonoBehaviour
     {
         None,
         Speed,
+        jump
     }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
