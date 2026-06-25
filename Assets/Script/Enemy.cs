9718548953d48bfd4085b6532535d6b1337c8c67
R1806 <student@KIC-GAME-C35>
2026-06-25 14:47:22 +0900

    敵攻撃処理の追加

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class Enemy : MonoBehaviour
 {
@@ -9,11 +10,13 @@ public class Enemy : MonoBehaviour
     public Transform Player;
     public Transform model;
     public Transform firePoint;
+    Vector3 Distance;
+    public float attackRange = 10f;
 
     //private EnemySpawner spawner;
     //public GameObject nextEnemy;
 
-    private int direction=1;
+    private int direction = 1;
     private Vector3 StartPos;
     private LineRenderer line;
 
@@ -54,16 +57,27 @@ public class Enemy : MonoBehaviour
 
     void FeirLaser()
     {
-        line.SetPosition(0,firePoint.position);
+        line.SetPosition(0, firePoint.position);
         line.SetPosition(1, Player.position);
     }
     void Update()
     {
-   
+
 
         if (Player != null)
         {
-             FeirLaser();
+
+            float distance = Vector3.Distance(transform.position, Player.position);
+
+            bool isAttacking = distance <= attackRange;
+
+            line.enabled = isAttacking;
+
+            if (isAttacking)
+            {
+                FeirLaser();
+                LaserHitCheck();
+            }
 
             Vector3 targetPos = Player.position;
             targetPos.y = model.position.y;
@@ -71,41 +85,58 @@ public class Enemy : MonoBehaviour
             model.LookAt(targetPos);
 
             //Debug.Log(model.eulerAngles);
-
-
-
-            if (Keyboard.current.kKey.wasPressedThisFrame)
-
-            {
-
-                Destroy(gameObject);
-
-                //spawner.SpawnEnemy();
-            }
         }
-        
 
-    //    transform.Translate(Vector3.right * Speed * direction * Time.deltaTime);
 
-    //    if (transform.position.x > StartPos.x + moveRange)
-    //    {
-    //        direction = -1;
-    //    }
+        if (Keyboard.current.kKey.wasPressedThisFrame)
 
-    //    if(transform.position.x<StartPos.x-moveRange)
-    //    {
-    //        direction = 1;
-    //    }
+        {
+
+            Destroy(gameObject);
+
+            //spawner.SpawnEnemy();
+        }
+
+
+
+        //    transform.Translate(Vector3.right * Speed * direction * Time.deltaTime);
+
+        //    if (transform.position.x > StartPos.x + moveRange)
+        //    {
+        //        direction = -1;
+        //    }
+
+        //    if(transform.position.x<StartPos.x-moveRange)
+        //    {
+        //        direction = 1;
+        //    }
 
     }
 
-    public void TakeDamage(float  damage=1f)
+    public void TakeDamage(float damage = 1f)
     {
         Hp -= damage;
 
         if (Hp <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void LaserHitCheck()
+    {
+        Vector3 start = firePoint.position;
+        Vector3 end = Player.position;
+
+        Vector3 dir = (end - start).normalized;
+        float distance = Vector3.Distance(start, end);
+
+        if (Physics.Raycast(start, dir, out RaycastHit hit, distance))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                hit.transform.GetComponent<Player>().TakeDamage(10f * Time.deltaTime);
+            }
         }
     }
 }
