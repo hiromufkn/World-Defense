14e6847f469be1e8db1a4201554bef8181e4adb3
吹野拓夢 <kof2564720@stu.o-hara.ac.jp>
2026-09-14 15:05:10 +0900

    wallrunでのアニメーションの追加

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -175,7 +175,7 @@ public class Player : MonoBehaviour
                 break;
 
             case PlayerStatus.WallRun:
-                animator.Play("wallRun");
+                animator.Play("run");
                 break;
 
             case PlayerStatus.Jump:
